78147dda85d962a7f9e6c2a4bbe42abf0fc83c67
Lee Briggs <lee@leebriggs.co.uk>
2022-09-16 09:17:00 -0700

    fix python test file

--- examples/examples_py_test.go
+++ examples/examples_py_test.go
@@ -4,15 +4,26 @@
 package examples
 
 import (
+	//"path"
 	"path/filepath"
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v3/testing/integration"
 )
 
+// func TestAccWebserver(t *testing.T) {
+// 	test := getPythonBaseOptions(t).
+// 		With(integration.ProgramTestOptions{
+// 			Dir: path.Join(getCwd(t), "python/server"),
+// 		})
+
+// 	integration.ProgramTest(t, &test)
+// }
+
 func getPythonBaseOptions(t *testing.T) integration.ProgramTestOptions {
-	base := getBaseOptions()
+	base := getBaseOptions(t)
 	basePython := base.With(integration.ProgramTestOptions{
+		ExpectRefreshChanges: true,
 		Dependencies: []string{
 			filepath.Join("..", "sdk", "python", "bin"),
 		},
